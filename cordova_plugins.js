557ac2385af4854e6b88d1723afed4efb88759c1
Liming Xie <liming.xie@gmail.com>
2014-10-07 13:37:19 +0800

    add new product, mMedia plugin

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -13,6 +13,20 @@ var cordova_plugins = {
 		github_url : "https://github.com/floatinghotpot/cordova-admob-pro",
 		paypal_url : "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NRKTFZ88Q9KTW"
 	},
+	"com.rjfun.cordova.mmedia" : {
+		intro : "Millennial Media is known for being the first mobile ad network that went to the next level through an initial public offering. This platform provides a wealth of research and intelligence, and you can use it to quickly set up a campaign for advertisers to compete for your business.",
+		signup_url : "http://www.mmedia.com/",
+		onsale : true,
+		cat : "ad",
+		name : "mMedia (Millennial Media) PluginPro",
+		desc : "Using this cordova plugin for mMedia (Millennial Media), you can easily display Banner and full screen Video Ad with single line of javascript code.",
+		ios : true,
+		android : true,
+		win : false,
+		price : 20.00,
+		github_url : "https://github.com/floatinghotpot/cordova-plugin-mmedia",
+		paypal_url : "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XJYYFV9XFF9U4"
+	},
 	"com.rjfun.cordova.plugin.iad" : {
 		intro : "Apple's iAd service. With iAd, you can get your message out to the millions of people using iPhone and iPad.",
 		onsale : true,
@@ -37,7 +51,7 @@ var cordova_plugins = {
 		android : true,
 		win : false,
 		price : 20.00,
-		github_url : "https://github.com/floatinghotpot/cordova-mobfox-pro",
+		github_url : "https://github.com/floatinghotpot/cordova-plugin-mobfox",
 		paypal_url : "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=HJCR6JFWTNNFW"
 	},
 	"com.rjfun.cordova.plugin.flurry" : {
@@ -54,19 +68,19 @@ var cordova_plugins = {
 		github_url : "https://github.com/floatinghotpot/cordova-plugin-flurry",
 		paypal_url : "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=C654HGZVC43T4"
 	},
-	"com.rjfun.cordova.mmedia" : {
-		intro : "Millennial Media is known for being the first mobile ad network that went to the next level through an initial public offering. This platform provides a wealth of research and intelligence, and you can use it to quickly set up a campaign for advertisers to compete for your business.",
-		signup_url : "http://www.millennialmedia.com/",
+	"com.rjfun.cordova.vungle" : {
+		intro : "Not all ads are boring to users. Think of all the people who pay attention to the amazing ads that air during the Super Bowl, for example. Vungle is devoted to helping you monetize your app with interesting and engaging video ads that your end users will enjoy viewing.",
+		signup_url : "http://www.vungle.com/",
 		onsale : false,
 		cat : "ad",
-		name : "Millennial Media Plugin",
-		desc : "Using this cordova plugin for Millennial Media, you can easily display Banner, Interstitial and Video Ad with single line of javascript code.",
+		name : "Vungle Plugin",
+		desc : "Using this cordova plugin for Vungle, you can easily display Video Ad with single line of javascript code.",
 		ios : true,
 		android : true,
 		win : false,
 		price : 20.00,
-		github_url : "https://github.com/floatinghotpot/cordova-plugin-mmedia",
-		paypal_url : "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XJYYFV9XFF9U4"
+		github_url : "https://github.com/floatinghotpot/cordova-plugin-vungle",
+		paypal_url : "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=HDGF755KQQWRY"
 	},
 	"com.rjfun.cordova.tapjoy" : {
 		intro : "If you have developed a mobile game, you'll want to consider Tapjoy because of its expertise with in-app purchases. It enables your users to earn virtual currency that they can cash in for content in more than 20,000 games to date.",
@@ -81,20 +95,6 @@ var cordova_plugins = {
 		price : 20.00,
 		github_url : "https://github.com/floatinghotpot/cordova-plugin-tapjoy",
 		paypal_url : "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=WRT588EJ4BHJE"
-	},
-	"com.rjfun.cordova.vungle" : {
-		intro : "Not all ads are boring to users. Think of all the people who pay attention to the amazing ads that air during the Super Bowl, for example. Vungle is devoted to helping you monetize your app with interesting and engaging video ads that your end users will enjoy viewing.",
-		signup_url : "http://www.vungle.com/",
-		onsale : false,
-		cat : "ad",
-		name : "Vungle Plugin",
-		desc : "Using this cordova plugin for Vungle, you can easily display Video Ad with single line of javascript code.",
-		ios : true,
-		android : true,
-		win : false,
-		price : 20.00,
-		github_url : "https://github.com/floatinghotpot/cordova-plugin-vungle",
-		paypal_url : "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=HDGF755KQQWRY"
 	},
 	"com.rjfun.cordova.plugin.appleiap" : {
 		onsale : true,
